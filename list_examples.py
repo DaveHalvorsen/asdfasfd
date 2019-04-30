ae78dc3a73f0449257e7ae9c625615f046cab309
Dave Halvorsen <daviqd.halvorsen@gmail.com>
2019-04-30 12:45:00 -0700

    Add files via upload
    this is a description

---
+++ list_examples.py
@@ -0,0 +1,8 @@
+job_applicants = ["John", "Brady", "Thomas"]
+heights = [12, 34, 53, 23]
+ints_and_strings = [1, 2, 3, "four", "five"]
+empty_list = []
+
+def text(print_this):
+	print(print_this)
+text("hello")
